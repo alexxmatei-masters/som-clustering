4cb354cdc3deb60c3633d46425aa0334f0c2edfa
alexxmatei <alexxmatei@gmail.com>
2023-05-03 20:41:30 +0300

    Change plotted iterations

--- src/main.rs
+++ src/main.rs
@@ -338,7 +338,7 @@ fn main() {
             }
         }
 
-        if (index % 1000 == 0) {
+        if (index == 9999 || index == 0) {
             let mut plot2 = BitMapBackend::new(&path2, (600, 600)).into_drawing_area();
             draw_points(&points, &mut plot2);
             draw_lines(&som_network, &mut plot2);
